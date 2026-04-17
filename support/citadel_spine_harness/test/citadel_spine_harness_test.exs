076e920843209df6753bccdca1ec8fba5eefb829
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-04-16 18:01:56 -1000

    Drop ops assurance dependency from harness

--- support/citadel_spine_harness/test/citadel_spine_harness_test.exs
+++ support/citadel_spine_harness/test/citadel_spine_harness_test.exs
@@ -210,6 +210,26 @@ defmodule StackLab.CitadelSpineHarnessTest do
     end)
   end
 
+  test "harness mix project does not declare or reference the deprecated mezzanine ops_assurance package" do
+    deps = MixProject.project()[:deps]
+
+    refute Enum.any?(deps, fn
+             {:mezzanine_ops_assurance, _opts} -> true
+             {:mezzanine_ops_assurance, _requirement, _opts} -> true
+             _other -> false
+           end)
+
+    harness_root = Path.expand("..", __DIR__)
+
+    harness_root
+    |> Path.join("lib/**/*.ex")
+    |> Path.wildcard(match_dot: true)
+    |> Enum.each(fn path ->
+      refute File.read!(path) =~ "Mezzanine.Assurance",
+             "#{path} still references Mezzanine.Assurance"
+    end)
+  end
+
   test "remote spine startup returns only after the remote service is callable" do
     try do
       remote = RemoteSupport.start_remote_spine!(:startup_probe)
